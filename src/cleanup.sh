aef3c66f3923c44c18cb4733d4fd6d7c4d4a0571
Scepanovic Sanja <sanja.scepanovic@nokia-bell-labs.com>
2018-10-16 00:33:38 +0100

    First commit

--- src/cleanup.sh
+++
@@ -1,8 +0,0 @@
-rm labels/* -r
-rm training-pngs/* -r
-rm label-mask-tiffs/* -r
-rm label-cropped-tiffs/* -r
-rm original-scaled-tiffs/* -r
-rm original-dec-tiffs/* -r
-rm orig-cropped-tiffs/* -r
-rm orig-tiffs-to-rgb/* -r
